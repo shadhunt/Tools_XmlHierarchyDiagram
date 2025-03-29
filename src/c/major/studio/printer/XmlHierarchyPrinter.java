d75f3b688ee6b47fc02daf070c18d849211751b2
shadhunt <shaoyun.claude@gmail.com>
2025-03-29 02:28:35 -0400

    right way to output hierarchy in a single file

--- src/c/major/studio/printer/XmlHierarchyPrinter.java
+++ src/c/major/studio/printer/XmlHierarchyPrinter.java
@@ -6,10 +6,41 @@ import java.util.Map.Entry;
 
 public class XmlHierarchyPrinter {
 	
+	private boolean isDebugged=false;
 	private Map<String, String> xmlValueMap;
 	public XmlHierarchyPrinter(Map<String, String> xmlValueMap) {
 		this.xmlValueMap = xmlValueMap;
 		// TODO Auto-generated constructor stub
+	}
+	
+	public void processPrinting()
+	{
+		for(Entry<String, String> entry : xmlValueMap.entrySet())
+		{
+			if(entry.getValue()==null)
+			{
+				printHierarchy(entry.getKey(),0);
+			}
+		}
+	}
+	
+	public void printHierarchy(String key, int level)
+	{
+		if (isDebugged) {
+			System.out.println("level:"+level);
+		}
+		for(int n=0;n<level;n++)
+		{
+			System.out.print("  ");
+		}
+		System.out.println("|--"+key);
+		for(Entry<String, String> entry : xmlValueMap.entrySet())
+		{
+			if(entry.getValue()!=null && entry.getValue().equals(key))
+			{
+				printHierarchy(entry.getKey(), ++level);
+			}
+		}
 	}
 	/* this ia reversed way of output the parent and child
 	public void processPrinting()
